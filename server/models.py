30376f2c1fc04c9510236cbe7612c87ade2c6343
Monalisa Onyango <+>
2024-10-04 17:41:01 +0300

    serialization

--- server/models.py
+++ server/models.py
@@ -17,8 +17,23 @@ metadata = MetaData(naming_convention=convention)
 db = SQLAlchemy(metadata=metadata)
 
 
-class Zookeeper(db.Model):
+# class Zookeeper(db.Model, SerializerMixin):
+#     __tablename__ = 'zookeepers'
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String, unique=True)
+#     birthday = db.Column(db.Date)
+
+#     animals = db.relationship('Animal', back_populates='zookeeper')
+
+#     serialize_rules = ('-animals.zookeeper',),
+
+
+class Zookeeper(db.Model, SerializerMixin):
     __tablename__ = 'zookeepers'
+    
+    serialize_only = ('id', 'name', 'animals.name', 'animals.species',)
+    serialize_rules = ()
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, unique=True)
@@ -27,7 +42,8 @@ class Zookeeper(db.Model):
     animals = db.relationship('Animal', back_populates='zookeeper')
 
 
-class Enclosure(db.Model):
+
+class Enclosure(db.Model, SerializerMixin):
     __tablename__ = 'enclosures'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -36,8 +52,10 @@ class Enclosure(db.Model):
 
     animals = db.relationship('Animal', back_populates='enclosure')
 
+    serialize_rules = ('-animals.enclosure',)
 
-class Animal(db.Model):
+
+class Animal(db.Model, SerializerMixin):
     __tablename__ = 'animals'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -49,6 +67,8 @@ class Animal(db.Model):
 
     enclosure = db.relationship('Enclosure', back_populates='animals')
     zookeeper = db.relationship('Zookeeper', back_populates='animals')
+
+    serialize_rules = ('-zookeeper.animals', '-enclosure.animals',)
 
     def __repr__(self):
         return f'<Animal {self.name}, a {self.species}>'
